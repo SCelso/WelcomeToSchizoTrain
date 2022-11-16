8decb03e18088ff15b3ed5f33a275606f54cdc40
SCelso <sergio.celso.sanchez@gmail.com>
2022-11-16 13:30:13 +0100

    Train and Regulation center

--- model/Train.py
+++ model/Train.py
@@ -1,1 +1,15 @@
 class Train:
+    def __init__(self,train_status,position,departure_time,section,finished,speed):
+        self.__status= train_status
+        self.__position=position
+        self.__departure_time=departure_time
+        self.__section=section
+        self.__finished=finished
+        self.__speed=speed
+
+    def update():
+        return
+    def finish():
+        return
+    def advance():
+        return
